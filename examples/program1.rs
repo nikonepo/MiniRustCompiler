720e3a53424e216f0e86ddb0ef3ba395b497ed6b
nikonepo <nikolainepo@gmail.com>
2025-05-22 01:06:43 +0300

    Add example

---
+++ examples/program1.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut var1 int = 100;
+    let mut var2 int = 200;
+    let mut var3 int = (var1 + var2) * 2 + 3 * var1;
+
+    print(50);
+    print(80);
+
+    let var1 int = 100;
+
+    let var2 int;
+}
